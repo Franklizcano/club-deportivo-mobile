36f5115afcb2a09b3d57c4353e827f9d85de33e5
FrankLizcano <80275021+Franklizcano@users.noreply.github.com>
2025-09-29 19:03:36 -0300

    rename MainActivity to Login and add MenuPrincipal activity layout

--- app/src/main/java/com/aislados/clubdeportivo/Login.kt
+++ app/src/main/java/com/aislados/clubdeportivo/Login.kt
@@ -1,5 +1,6 @@
 package com.aislados.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -9,18 +10,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.aislados.clubdeportivo.database.UserDAO
 import com.aislados.clubdeportivo.model.User
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import kotlin.text.clear
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val username = findViewById<TextInputEditText>(R.id.username)
+        username.requestFocus()
 
         val userTable = UserDAO(this)
         val userTest = User("admin", "admin")
@@ -28,12 +34,21 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            val username = findViewById<TextInputEditText>(R.id.username).text.toString()
-            val password = findViewById<TextInputEditText>(R.id.password).text.toString()
-            if (userTable.existsUser(username, password)) {
-                Toast.makeText(this, "Usuario encontrado", Toast.LENGTH_SHORT).show()
+            val password = findViewById<TextInputEditText>(R.id.password)
+
+            if (userTable.existsUser(username.text.toString(), password.text.toString())) {
+                Snackbar.make(findViewById(R.id.main), "¡Bienvenido!", Snackbar.LENGTH_SHORT).show()
+                Intent(this, MenuPrincipal::class.java)
+                    .also { startActivity(it) }
             } else {
-                Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_SHORT).show()
+                Snackbar.make(findViewById(R.id.main), "Usuario o contraseña incorrectos", Snackbar.LENGTH_LONG)
+                    .setAction("Reintentar") {
+                        // Limpiar campos
+                        username.text?.clear()
+                        password.text?.clear()
+                        username.requestFocus()
+                    }
+                    .show()
             }
         }
     }
