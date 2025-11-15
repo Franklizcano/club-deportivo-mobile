aeb9d09b0f924b9b61248cea493b2463cd758de6
FrankLizcano <80275021+Franklizcano@users.noreply.github.com>
2025-11-15 20:33:25 -0300

    refactor: update LocalDateParceler to use ISO_LOCAL_DATE format for parsing and writing LocalDate

--- app/src/main/java/com/aislados/clubdeportivo/model/LocalDateParceler.kt
+++ app/src/main/java/com/aislados/clubdeportivo/model/LocalDateParceler.kt
@@ -3,13 +3,14 @@ package com.aislados.clubdeportivo.model
 import android.os.Parcel
 import kotlinx.parcelize.Parceler
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object LocalDateParceler : Parceler<LocalDate> {
     override fun create(parcel: Parcel): LocalDate {
-        return LocalDate.parse(parcel.readString())
+        return LocalDate.parse(parcel.readString(), DateTimeFormatter.ISO_LOCAL_DATE)
     }
 
     override fun LocalDate.write(parcel: Parcel, flags: Int) {
-        parcel.writeString(this.toString())
+        parcel.writeString(this.format(DateTimeFormatter.ISO_LOCAL_DATE))
     }
 }
