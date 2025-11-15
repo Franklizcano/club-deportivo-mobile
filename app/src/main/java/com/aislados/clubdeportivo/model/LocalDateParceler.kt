deac6319ebbfb12aa67740d24eb04f94186df15c
FrankLizcano <80275021+Franklizcano@users.noreply.github.com>
2025-11-15 20:33:44 -0300

    refactor: simplify LocalDateParceler by removing explicit date formatting

--- app/src/main/java/com/aislados/clubdeportivo/model/LocalDateParceler.kt
+++ app/src/main/java/com/aislados/clubdeportivo/model/LocalDateParceler.kt
@@ -3,14 +3,13 @@ package com.aislados.clubdeportivo.model
 import android.os.Parcel
 import kotlinx.parcelize.Parceler
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 object LocalDateParceler : Parceler<LocalDate> {
     override fun create(parcel: Parcel): LocalDate {
-        return LocalDate.parse(parcel.readString(), DateTimeFormatter.ISO_LOCAL_DATE)
+        return LocalDate.parse(parcel.readString())
     }
 
     override fun LocalDate.write(parcel: Parcel, flags: Int) {
-        parcel.writeString(this.format(DateTimeFormatter.ISO_LOCAL_DATE))
+        parcel.writeString(this.toString())
     }
 }
